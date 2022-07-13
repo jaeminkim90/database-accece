c7e35fbda78819e46c854e3bf3096bc683ba2c68
Buddy-Jaemin Kim <jaeminkim90@gmail.com>
2022-07-13 09:22:01 +0900

    feat: Hikari CP를 사용하여 커넥션 연결하는 방식을 구현한다

--- src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,14 +23,21 @@ class MemberRepositoryV1Test {
     @BeforeEach // 각각의 테스트 메서드 호출 전에 실행된다
     void beforeEach() {
         // 기본 DriverManager - 항상 새로운 커넥션 획득
-        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        // DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+        // 커넥션 풀링을 사용하는 방법
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
         repository = new MemberRepositoryV1(dataSource);
 
     }
 
 
     @Test
-    void crud() throws SQLException {
+    void crud() throws SQLException, InterruptedException {
 
         // save
         Member member = new Member("memberV8", 10000);
@@ -52,6 +61,8 @@ class MemberRepositoryV1Test {
         // 삭제 검증 방법 : NoSuchElementException이 발생하는지 확인한다
         assertThatThrownBy(() -> repository.findById(member.getMemberId()))
             .isInstanceOf(NoSuchElementException.class);
+
+        Thread.sleep(1000);
 
     }
 }
