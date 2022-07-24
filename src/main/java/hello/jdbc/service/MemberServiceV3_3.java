e2da2e0340cace6c097954ee28ab41daf779d672
Buddy-Jaemin Kim <jaeminkim90@gmail.com>
2022-07-24 17:21:09 +0900

    feat: 스프링이 제공하는 트랜잭션 AOP를 사용하여 트랜잭션 적용한다

--- src/main/java/hello/jdbc/service/MemberServiceV3_3.java
+++ src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -11,35 +11,20 @@ import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- *  트랜잭션 - 트랜잭션 템플릿
- *  Service에서 Spring이 제공하는 TransactionManager를 연결하는 코드 실습
+ *  트랜잭션 - @Transactional AOP
  */
 @Slf4j
-public class MemberServiceV3_2 {
+public class MemberServiceV3_3 {
 
-    private final TransactionTemplate txTemplate;
     private final MemberRepositoryV3 memberRepository; // DB 접근을 위해 repository 주입 받음
 
-    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
-        // PlatformTransactionManager을 파라미터에 넣어서 TransactionTemplate를 만들 수 있다. -> 유연성이 생기기 때문에 외부 주입방식보다 좋다.
-        this.txTemplate = new TransactionTemplate(transactionManager);
+    public MemberServiceV3_3(MemberRepositoryV3 memberRepository) {
         this.memberRepository = memberRepository;
     }
 
     @Transactional
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-
-        // 기존에 getTransaction()을 이용해 status를 얻어 트랜잭션매니저 설정에 사용했던 것을 아래와 같이 간소화 할 수 있다
-        // 트랜잭션매니저를 이용한 commit과 rollback 기능까지 모두 대체가 가능하다
-        // executeWithoutResult()가 실행되면 해당 코드 안에서 트랜잭션을 시작하고 아래 비즈니스로직을 수행한다.
-        // 성공적으로 작업이 완료되면 commit을 그렇지 않으면 rollback을 한다
-        txTemplate.executeWithoutResult((status) -> {
-            try {
-                bizLogic(fromId, toId, money);
-            } catch (SQLException e) {
-                throw new IllegalStateException(e);
-            }
-        });
+        bizLogic(fromId, toId, money);
     }
 
     /**
