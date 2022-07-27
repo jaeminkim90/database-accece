b2dbc14a1bc969bc199674c346b154677de1ac39
Buddy-Jaemin Kim <jaeminkim90@gmail.com>
2022-07-27 09:21:38 +0900

    test: Unchecked 예외 사용시 throws 예외 선언 없이도 컴파일 에러가 발생하지 않는다

--- src/test/java/hello/jdbc/exception/basic/UncheckedTest.java
+++ src/test/java/hello/jdbc/exception/basic/UncheckedTest.java
@@ -1,9 +1,26 @@
 package hello.jdbc.exception.basic;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class UncheckedTest {
+
+    @Test
+    void unchecked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void unchecked_throw() {
+        Service service = new Service();
+        Assertions.assertThatThrownBy(() -> service.callThrow())
+            .isInstanceOf(MyUncheckedException.class);
+
+    }
 
     /**
      * RuntimeException을 상속받은 예외는 언체크 예외가 된다.
@@ -32,7 +49,14 @@ public class UncheckedTest {
                 // 예외 로직 처리
                 log.info("예외 처리, message = {}", e.getMessage(), e);
             }
+        }
 
+        /**
+         * 예외를 잡지 않아도 된다. 자연스럽게 상위로 넘어간다.
+         * 체크 예외와 다르게 throws 예외 선언을 하지 않아도 된다.
+         */
+        public void callThrow() {
+            repository.call();
         }
 
     }
