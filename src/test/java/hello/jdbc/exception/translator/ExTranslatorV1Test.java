091805b19c927b1ec84b20ee70c8a84a2f1f9e67
Buddy-Jaemin Kim <jaeminkim90@gmail.com>
2022-07-30 18:25:56 +0900

    test: 중복 id 발생 시, 예외를 전환해서 처리하는 테스트 코드를 구현한다.

--- src/test/java/hello/jdbc/exception/translator/ExTranslatorV1Test.java
+++ src/test/java/hello/jdbc/exception/translator/ExTranslatorV1Test.java
@@ -7,16 +7,38 @@ import java.util.Random;
 
 import javax.sql.DataSource;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.support.JdbcUtils;
 
+import hello.jdbc.connection.ConnectionConst;
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.ex.MyDbException;
 import hello.jdbc.repository.ex.MyDuplicateKeyException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import static hello.jdbc.connection.ConnectionConst.*;
+
 @Slf4j
 public class ExTranslatorV1Test { // 예외를 전환하는 V1 Test
+
+    Repository repository;
+    Service service;
+
+    @BeforeEach
+    void init() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        repository = new Repository(dataSource);
+        service = new Service(repository);
+    }
+
+    @Test
+    void duplicateKeySave() {
+        service.create("myid");
+        service.create("myid"); // 같은 ID 저장 시도. 중복 id 에러 메시지를 발생시킨다.
+    }
 
     @Slf4j
     @RequiredArgsConstructor
@@ -34,11 +56,12 @@ public class ExTranslatorV1Test { // 예외를 전환하는 V1 Test
                 log.info("retryId={}", retryId);
                 repository.save(new Member(retryId, 0));
 
-            } catch (MyDbException e) {
+            } catch (MyDbException e) { // catch는 중복해서 잡을 수 있다
                 log.info("데이터 접근 계층 예외", e);
                 throw e;
             }
         }
+
         private String generateNewId(String memberId) {
             return memberId + new Random().nextInt(10000);
         }
@@ -68,6 +91,8 @@ public class ExTranslatorV1Test { // 예외를 전환하는 V1 Test
                     // 키 중복 예외 발생 시, 예외를 변경한다
                     throw new MyDuplicateKeyException(e);
                 }
+                throw new MyDbException(e); //  키 중복 이외의 모든 예외는 MyDbException으로 전환하여 던진다
+
             } finally {
                 JdbcUtils.closeStatement(pstmt); // PreparedStatement 반납
                 JdbcUtils.closeConnection(con); // Connection 반납
