6dff6661163c9b076f26166d6bfbb90263af24ca
Buddy-Jaemin Kim <jaeminkim90@gmail.com>
2022-07-30 15:13:56 +0900

    feat: Reposiroy 인터페이스를 의존하는 Service를 구현한다

--- src/main/java/hello/jdbc/service/MemberServiceV4.java
+++ src/main/java/hello/jdbc/service/MemberServiceV4.java
@@ -1,37 +1,34 @@
 package hello.jdbc.service;
 
-import java.sql.SQLException;
-
 import org.springframework.transaction.annotation.Transactional;
-
 import hello.jdbc.domain.Member;
-import hello.jdbc.repository.MemberRepositoryV3;
+import hello.jdbc.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 
 /**
  *  예외 누수 문제 해결
  *  SQLException 제거
  *
- *  MemberRepository
+ *  MemberRepository 인터페이스 의존
  */
 @Slf4j
 public class MemberServiceV4 {
 
-    private final MemberRepositoryV3 memberRepository; // DB 접근을 위해 repository 주입 받음
+    private final MemberRepository memberRepository; // MemberRepository 인터페이스에 의존한다
 
-    public MemberServiceV4(MemberRepositoryV3 memberRepository) {
-        this.memberRepository = memberRepository;
+    public MemberServiceV4(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository; // 생성자 주입도 인터페이스를 받는다
     }
 
     @Transactional
-    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+    public void accountTransfer(String fromId, String toId, int money) { // throws SQLException 제거가 가능(Repo에서 언체크로 변환됨)
         bizLogic(fromId, toId, money);
     }
 
     /**
      * 비즈니스 로직: member의 money update
      **/
-    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+    private void bizLogic(String fromId, String toId, int money) { // throws SQLException 제거가 가능(Repo에서 언체크로 변환됨)
         // 비즈니스 로직
         Member fromMember = memberRepository.findById(fromId); // 보내는 멤버
         Member toMember = memberRepository.findById(toId);
